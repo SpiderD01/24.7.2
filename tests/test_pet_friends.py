ec2f4cdf21d792417a806cde920d77add74fd0f4
SpiderD01 <130400735+SpiderD01@users.noreply.github.com>
2023-07-12 22:46:02 +0300

    Update test_pet_friends.py

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -19,3 +19,13 @@ def test_post_add_new_pet_without_photo(name='Тимофей', animal_type='ко
     status, result = pf.post_add_new_pet_without_photo(auth_key, name, animal_type, age)
     assert status == 200
     assert result['name'] == name
+
+def test_get_api_key_with_wrong_password_and_correct_mail(email=valid_email, password=invalid_password):
+    status, result = pf.get_app_key(email, password)
+    assert status == 403
+    assert 'key' not in result
+
+def test_get_api_key_with_wrong_email_and_correct_password(email=invalid_email, password=valid_password):
+    status, result = pf.get_app_key(email, password)
+    assert status == 403
+    assert 'key' not in result
