e30fde0ecc56ef1184f2c88a891bd0a42a0f50dc
SpiderD01 <130400735+SpiderD01@users.noreply.github.com>
2023-07-12 22:39:06 +0300

    Update test_pet_friends.py

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -13,3 +13,9 @@ def test_get_all_pets_with_valid_key(filter=''):
     status, result = pf.get_list_of_pets(auth_key, filter)
     assert status == 200
     assert len(result['pets']) > 0
+
+def test_post_add_new_pet_without_photo(name='Тимофей', animal_type='кот', age=1):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.post_add_new_pet_without_photo(auth_key, name, animal_type, age)
+    assert status == 200
+    assert result['name'] == name
